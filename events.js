45dbd2780fce36095b534496b696b37ca484bcaf
Emre <144052970+emreuyanik@users.noreply.github.com>
2023-11-02 23:10:30 +0300

    key

--- events.js
+++ events.js
@@ -44,3 +44,20 @@ addButton.addEventListener("click", () => {
   input.value = ""
   input.focus()
 })
+
+
+//? input elementi üzerinde iken klavyeden bir tusa basildigin tetiklenir.
+input.addEventListener("keyup", (event) => {
+    //   console.log(event)
+    //   console.log(event.target)
+    //   console.log(event.target.value)
+    //   console.log(event.keyCode)
+    //   console.log(event.code)
+  
+    //?  basilan tus enter ise
+    if (event.code === "Enter") {
+      //? add butonun click fonkisyonu cagiriyoruz
+      addButton.click()
+    }
+  })
+  
