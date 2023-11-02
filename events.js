0907ecf8b8d7ff17c246ee2c7b916b4e6314560e
Emre <144052970+emreuyanik@users.noreply.github.com>
2023-11-02 22:51:21 +0300

    first commit

---
+++ events.js
@@ -0,0 +1,46 @@
+//* ==============================================
+//*                  EVENTS
+//* ===============================================
+
+console.log("****** EVENTS *******")
+
+//? Selector variables
+const header = document.querySelector("header")
+const addButton = document.getElementById("btn")
+const input = document.querySelector("#input")
+const ul = document.querySelector("ul")
+
+//? 3. yontemle event tanimlandi
+//* mouse header 'ın üzerine geldiginde ilgili function cagrilir.
+header.onmouseover = function () {
+  header.style.backgroundColor = "red"
+  header.style.color = "white"
+}
+//* mouse header 'ın üzerinden ayrildiginda ilgili function cagrilir.
+header.onmouseout = function () {
+  header.style.backgroundColor = "transparent"
+  header.style.color = "black"
+}
+
+const yazdir = () => {
+  document.write("Hello from Turkey")
+}
+
+//? onload event'ı html ve css kodlarininin render edilmesinin hemen akabinde calisir.
+// window.onload = yazdir()
+
+//? window yüklendiginde input'a focuslan
+window.addEventListener("load", () => {
+  input.focus()
+})
+
+//? 4.Yontem (addeventlistener metodu) ile event tanimlandi
+//* add butonuna tiklanildgin event funksiyonu cagrilir
+addButton.addEventListener("click", () => {
+  const li = document.createElement("li")
+  const text = document.createTextNode(input.value)
+  li.appendChild(text)
+  ul.appendChild(li)
+  input.value = ""
+  input.focus()
+})
